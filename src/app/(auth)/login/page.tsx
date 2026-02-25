3834d2285e91b5016153acac323223231195f411
Leonardo Malta <leonardomalta@Leonardos-MacBook-Air.local>
2026-02-25 12:32:02 -0300

    fix: themes and /public

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { authOptions } from '@/app/api/auth/[...nextauth]/options'
 import { LoginCarousel } from '@/components/carousels/login-carousel'
+import { ModeToggle } from '@/components/ui/mode-toggle'
 
 export const metadata: Metadata = {
   title: 'Acesse sua Dashboard',
@@ -20,6 +21,9 @@ export default async function LoginPage() {
   return (
     <main className="w-full lg:flex lg:flex-row-reverse">
       <section className="relative flex h-screen flex-col items-center justify-center lg:w-2/4">
+        <div className="absolute right-4 top-4">
+          <ModeToggle />
+        </div>
         <div className="flex w-full max-w-lg flex-col gap-5 px-6">
           <div className="flex flex-col items-center">
             <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
